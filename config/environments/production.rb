7eea6bbb4d95fecbd3f0f4a55fa59bd38ef22942
Christian Stevens <stev1905@gmail.com>
2015-12-03 20:15:17 -0500

    production.rb file changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = falseS
+  config.active_record.dump_schema_after_migration = false
 
   #note to set to actual hostname 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
